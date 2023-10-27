eff062eb8e1f331387dc2e7e2f7d123971701675
ayrtonlemes <ayrton290502@gmail.com>
2023-10-27 11:27:11 -0400

    Separando em treino, validação e teste

--- Tarefa4/separacaoTrainValTest.py
+++ Tarefa4/separacaoTrainValTest.py
@@ -5,37 +5,38 @@ import random
 # Diretório raiz contendo as pastas de classes
 dataset_root = 'frequenciasDefinitivas'
 
-# Diretório de saída para o conjunto de treinamento
+# Diretório de saída 
 train_dir = 'train1'
-
-# Diretório de saída para o conjunto de teste
+val_dir = 'val1'
 test_dir = 'test1'
 
-# Proporção de imagens a serem usadas para treinamento (por exemplo, 80%)
-train_ratio = 0.8
+# Proporção de imagens a serem usadas
+train_ratio = 0.6
+val_ratio = 0.2
 
-# Lista de pastas (rótulos) no diretório raiz
+
 labels = os.listdir(dataset_root)
 
-# Loop através das pastas (rótulos)
 for label in labels:
     label_dir = os.path.join(dataset_root, label)
     images = os.listdir(label_dir)
     random.shuffle(images)
 
-    # Determina a quantidade de imagens para o treinamento e teste
+    # Determina a quantidade de imagens para o treinamento, validação e teste
     num_train = int(train_ratio * len(images))
-    
-    # Cria os diretórios de treinamento e teste
+    num_val = int(val_ratio * len(images))
+
     os.makedirs(os.path.join(train_dir, label), exist_ok=True)
+    os.makedirs(os.path.join(val_dir, label), exist_ok=True)
     os.makedirs(os.path.join(test_dir, label), exist_ok=True)
 
-    # Copia as imagens para os diretórios de treinamento e teste
     for i, image in enumerate(images):
         if i < num_train:
             dest_dir = os.path.join(train_dir, label)
+        elif i < num_train + num_val:
+            dest_dir = os.path.join(val_dir, label)
         else:
             dest_dir = os.path.join(test_dir, label)
         shutil.copy(os.path.join(label_dir, image), os.path.join(dest_dir, image))
 
-print("Divisão de treinamento e teste concluída.")
+print("Divisão de treinamento, validação e teste concluída.")
